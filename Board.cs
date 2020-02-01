e05555e191e5b646fd1dc5949763300352db398d
sahar laor <saharlaor@gmail.com>
2020-02-01 17:32:14 +0200

    Basic 8x8 game board that loads dynamically

---
+++ Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GameBoard
+{
+    class Board
+    {
+        private Button[,] gameBoard = new Button[8, 8];
+        public Board()
+        {
+            InitBoard(gameBoard);
+        }
+
+        public Button[,] GameBoard { get => gameBoard; set => gameBoard = value; }
+
+        public void InitBoard(Button[,] board)
+        {
+            bool isBlack = false;
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    Button btn = new Button();
+                    btn.Height = 50;
+                    btn.Width = 50;
+                    btn.Padding = new Thickness(0);
+                    // The width and height of the placement 
+                    btn.Margin = new Thickness(btn.Width * (x * 2 - (board.GetLength(0) - 1)), btn.Height * (y - board.GetLength(1) / 2) * 2, 0, 0);
+                    btn.Background = (isBlack ? Brushes.Black : Brushes.White);
+                    isBlack = !isBlack;
+
+                    board[x, y] = btn;
+                }
+                isBlack = !isBlack;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Board board = new Board();
+            Grid grid = this.Content as Grid;
+
+            foreach (Button btn in board.GameBoard)
+            {
+                btn.Click += Button_Click;
+                grid.Children.Add(btn);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
